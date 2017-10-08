a06c62d5c2c0e641f04553595cbdb020e138f03b
Dankou Mathis <math98dankou@gmail.com>
2017-10-08 16:26:32 +0200

    LOL

--- td1/15.Tableau.c
+++ td1/15.Tableau.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+// A
 void init_tab(int tab[], int l)
 {
 	int i;
@@ -9,6 +11,7 @@ void init_tab(int tab[], int l)
 	}
 }
 
+// B
 void prod_tab(int tab[], int l)
 {
 	int i;
@@ -20,6 +23,7 @@ void prod_tab(int tab[], int l)
 	printf("%d", res);
 }
 
+// C
 int min_tab(int tab[], int l)
 {
 	int i;
@@ -35,9 +39,92 @@ int min_tab(int tab[], int l)
 	return min;
 }
 
+// D
+void decal_tab(int tab[], int l)
+{
+	int i;
+	for(i=l-1;i>0;i--)
+	{
+		tab[i] = tab[i-1];
+	}
+	tab[0] = 0; 
+
+	//AFFICHAGE
+	for(i=0;i<l;i++)
+	{
+		printf("%d", tab[i]);
+	}
+}
+
+
+// E
+void insert_ttab(int tab_t[], int l, int value)
+{
+	int i=0;
+	int i_insert;
+	while(tab_t[i] <= value)
+	{
+
+		i++;
+	}
+	i_insert = i;
+	for(i=l-1;i>i_insert;i--)
+	{
+		tab_t[i] = tab_t[i-1]; 
+	}
+	tab_t[i_insert] = value;
+
+	//AFFICHAGE
+	for(i=0;i<l;i++)
+	{
+		printf("%d ", tab_t[i]);
+	}
+
+}
+
+//F
+void inv_tab(int tab[], int l)
+{
+	int temp;
+	int i;
+	for(i=0;i<=l/2;i++)
+	{
+		temp = tab[l-i-1];
+		tab[l-i-1] = tab[i];
+		tab[i] = temp;
+	}
+	//AFFICHAGE
+	for(i=0;i<l;i++)
+	{
+		printf("%d ", tab[i]);
+	}
+}
+
+//G BUGE~~
+void suppr_doubl_tab(int tab[], int l)
+{
+	int i,j;
+	int value;
+	for(i=0;i<l;i++)
+	{
+		value = tab[i];
+		for(j=i+1;j<l;j++)
+		{
+			if(tab[j] == value)
+			{
+				tab[j] = -tab[j];
+			}
+		}
+	}
+	//AFFICHAGE
+	for(i=0;i<l;i++)
+	{
+		printf("%d ", tab[i]);
+	}
+}
 
 int main()
 {
-	int tab[5] = {7,1,3,5,2};
-	min_tab(tab, 5);
+	int tab[5] = {9,9,9,11,11};
+	suppr_doubl_tab(tab, 5);
 }
