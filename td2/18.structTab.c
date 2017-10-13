78c334ff8849aa8a274ea721df911eabf0cdb0da
Dankou Mathis <math98dankou@gmail.com>
2017-10-14 00:18:48 +0200

    avancage

--- td2/18.structTab.c
+++ td2/18.structTab.c
@@ -19,7 +19,7 @@ void initTab(tableau *t)
 {
 	int i;
 	(*t).taille = 10;
-	for(i=0;i<t.taille;i++)
+	for(i=0;i<(*t).taille;i++)
 	{
 		(*t).tab[i] =  alea(20);
 	}
@@ -30,13 +30,13 @@ void afficheTab(tableau t)
 	int i;
 	for(i=0;i<t.taille;i++)
 	{
-		printf("%d\n", t.tab[i]);
+		printf("%d / ", t.tab[i]);
 	}
 }
 
-int produitTab(tableau t)
+unsigned int produitTab(tableau t)
 {
-	int p;
+	unsigned int p;
 	int i;
 	p = 1;
 
@@ -47,16 +47,80 @@ int produitTab(tableau t)
 	return p;
 }
 
+int minTab(tableau t)
+{
+	int i;
+	int max = t.tab[0];
+	for(i=1;i<t.taille;i++)
+	{
+		if(max < t.tab[i])
+		{
+			max = t.tab[i];
+		}	
+	}
+	return max;
+}
+
+void decalTab(tableau *t)
+{
+
+	int i;
+	(*t).taille++;
+	for(i=(*t).taille;i>0;i--)
+	{
+		(*t).tab[i] = (*t).tab[i-1];
+	}
+	(*t).tab[0] = 0;
+}
+
+void triTab(tableau *t)
+{
+	int j,i,tmp;
+	for(j=0;j<(*t).taille-1;j++)
+	{
+		for(i=0;i<(*t).taille-1;i++)
+		{
+			if((*t).tab[i] > (*t).tab[i+1] )
+			{
+				tmp = (*t).tab[i];
+				(*t).tab[i] = (*t).tab[i+1];
+				(*t).tab[i+1] = tmp;
+
+			}
+		}
+	}
+}
+
+
+void inserTabTri(tableau *t, int value)
+{
+	int j,i;
+	j=0;
+	while(value >= (*t).tab[j])
+	{	
+		j++;
+	}
+	(*t).taille++;
+	for(i=(*t).taille;i>j;i++)
+	{
+		(*t).tab[i] = (*t).tab[i-1];		
+	}
+	(*t).tab[j] = value;
+}
+
 int main()
 {
-	int a;
 	time_t t;
 	tableau tab;
 	
 	srand((unsigned int) time(&t));
 	
 	initTab(&tab);
+	triTab(&tab);
+	inserTabTri(&tab, 5);
 	afficheTab(tab);
+	printf("%d", tab.taille);
+
 
 	return (0);
 }
