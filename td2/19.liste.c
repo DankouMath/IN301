fdf29188eed95db08ec114d1e479e6d051e9e0dd
Dankou Mathis <math98dankou@gmail.com>
2017-11-02 17:57:45 +0100

    upload

--- td2/19.liste.c
+++ td2/19.liste.c
@@ -85,27 +85,176 @@ Liste insereFin(Liste l, int n)
 	return l2;
 }
 
-Liste inserTrie(Liste l, int n)
+Liste insereTrie(Liste l, int n)
 {
+	Liste l2;
 	Liste ltmp = creerElem(n);
-	if(estVide(l) || n <l->val )
+	if(estVide(l) || n < l->val )
+	{
 		return insereDeb(l,n);
+	}
 	l2 = l;
-	while(!estVide(l->suiv) && l->suiv->val  < n)
+	while(!estVide(l->suiv) && l->val < n)
 	{
 		l = l->suiv;
 	}
-	ltmp = creerElem(n);
+	l->suiv = creerElem(n);
 	ltmp = l->suiv;
        	l->suiv = ltmp;
-	return l2;	
+	return l2;
+}
+
+Liste insereTrieR(Liste l, int n)
+{
+	if(estVide(l) || n < l->val)
+	{
+			return insereDeb(l,n);
+	}
+	l->suiv = insereTrieR(l->suiv,n);
+	return l;
+}
+
+int estTrie(Liste l)
+{
+	while(!estVide(l) && !estVide(l->suiv))
+	{
+		if(l->val > l->suiv->val)
+		{
+			return 0;
+		}
+		else
+		{
+			l = l->suiv;
+		}
+		return 1;
+	}
+}
+
+int estTrieR(Liste l)
+{
+	if(estVide(l) || estVide(l->suiv))
+	{
+		return 1;
+	}
+	if(l->val > l->suiv->val)
+	{
+		return 0;
+	}
+	else
+	{
+		estTrieR(l->suiv);
+	}
+}
+//D
+int nbElemListe(Liste l)
+{
+	int i=0;
+	while(!estVide(l))
+	{
+		l = l->suiv;
+		i++;
+	}
+	return i;
+}	
+
+//E
+int recherElem(Liste l, int x)
+{
+	while(!estVide(l))
+	{
+		if(l->val == x)
+			return 1;
+		l = l->suiv;
+	}
+	return 0;
+}
+
+//F
+Liste supprElem(Liste l, int n)
+{
+	Liste lcour, lpred;
+	if(estVide(l)) return l;
+	if(l->val == n)
+	{
+		lcour = l->suiv;
+		free(l);
+		return lcour;
+	}
+	lpred = l; lcour = l->suiv;
+	while(!estVide(lcour))
+	{
+		if(lcour->val==n)
+		{
+			lpred->suiv = lcour->suiv;
+			free(lcour);
+			return l;
+		}
+		lpred = lcour;
+		lcour = lcour->suiv;
+	}
+	return l;
+}
+
+Liste supprElemR(Liste l, int n)
+{
+	Liste lcour;
+	if(estVide(l)) return l;
+	if(l->val == n)
+	{
+		lcour = l->suiv;
+		free(l);
+		return lcour;
+	}
+	l->suiv = supprElemR(l->suiv, n);
+	return l;
+}
+
+//G
+Liste concatListe(Liste l, Liste l2)
+{
+	if(estVide(l)) return l2
+	Liste base = l;
+	while(!estVide(l->suiv))
+	{
+		l = l->suiv;
+	}
+	l->suiv = l2; 
+	return base;
+}
+
+Liste concatListeR(liste l1, liste l2)
+{
+	if(estVide(l1)) return l2;
+	l1->suiv = concatListR(l1->suiv, l2)
+	return l1;
+}
+
+//H
+Liste fusion(Liste l1, Liste l2)
+{
+	if(estVide(l1)) return l2;
+	if(estVide(l2)) return l1;
+	if(l1->val < l2->val)
+	{
+		l1->suiv = fusion(l1->suiv, l2);
+		return l1;
+	}
+	else
+	{
+		l2->suiv = fusion(l1,l2->suiv);
+		return l2;
+	}
 }
 
 int main()
 {
 	Liste l = creerListe();
-	l = creerElem(3);
-	l = insereFin(l,5);
+	Liste l2 = creerListe();
+	l = insereDeb(l,2);
+	l = insereDeb(l,1);
+	affiche(l);
+	printf("\n");
+	concatListe(l, l2);
 	affiche(l);
 	l = libere(l);
 	return 0;
